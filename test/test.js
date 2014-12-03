16b30a641aef20e67fc6b9f64d09cd480957e801
Luigi Pinca <luigipinca@gmail.com>
2014-12-03 15:37:07 +0100

    [codestyle] Removing trailing whitespaces

--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
 'use strict';
 
 var Adapter = require('../')
-  , expect = require('expect.js')
+  , expect = require('expect.js');
 
 describe('primus-rooms-adapter', function () {
-
   it('should have required methods', function () {
     var adapter = new Adapter();
     expect(adapter.set).to.be.a('function');
     expect(adapter.get).to.be.a('function');
-    expect(adapter.del).to.be.a('function');        
+    expect(adapter.del).to.be.a('function');
     expect(adapter.broadcast).to.be.a('function');
     expect(adapter.clients).to.be.a('function');
     expect(adapter.empty).to.be.a('function');
@@ -17,5 +16,4 @@ describe('primus-rooms-adapter', function () {
     expect(adapter.clear).to.be.a('function');
     expect(adapter.wildcard).to.be.an('object');
   });
-
 });
